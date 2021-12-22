37a92990c5ed8a2a51213b85dad686da42a7f990
王晓东 <public@cmmr.com.cn>
2021-12-22 16:41:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //app.js
+// 引入跳转js
+import router from '/utils/router.js';
 App({
   data: {
     //type 0复制  1粘贴   resType 资源类型 0图片 1视频 2音频
@@ -21,6 +23,87 @@ App({
      wx.setStorageSync('bgColor','blue')
      wx.setStorageSync('bgColorUi','#0081ff')
   },
+
+   /**
+ * 封装wx.request请求
+ * method： 请求方式
+ * url: 请求地址
+ * data： 要传递的参数
+ * callback： 请求成功回调函数
+ * errFun： 请求失败回调函数
+ **/
+ wxRequest(method, url, data,seesionid, callback, errFun) {
+  wx.showLoading({
+    title:'数据加载中'
+  });
+  wx.request({
+   url: url,
+   method: method,
+   data: data,
+   header: {
+    'content-type': method == 'GET'?'application/json':'application/x-www-form-urlencoded',
+    'Accept': 'application/json',
+    "cookie":seesionid
+   },
+   dataType: 'json',
+   success: function (res) { 
+     console.log(res)
+     //超时  重新登录
+    if(res.header["sessionstatus"] == "timeout"){
+      //var that = this
+      const app = getApp();//that.getApp()
+      //console.log(app)
+      wx.request({
+        url : app.globalData.requestUrl+'/wehcat/api/memberMange/silenceuserLogin',
+        method : "POST",
+        data: {
+          terminalUserId:app.terminalUserId,
+          openid:app.openid
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded',
+         },
+         dataType: 'json',
+         success: function (res) {
+           console.log(res)
+          if (res.data.status == 'success'){
+            var seesionId =res.header["Set-Cookie"]; 
+            app.seesionId = seesionId
+            console.log(app.seesionId)
+            app.wxRequest(method, url, data,seesionId, callback, errFun)
+          }else{
+            if(res.data.path == "jumpToLogin"){
+              wx.showToast({
+                title: res.data.message,
+                icon: 'none',
+                duration: 2000,
+                mask: true
+              })
+              setTimeout(() => {
+                router.navigateTo({
+                  url: "/pages/functionPage/functionPage"
+                }) 
+              }, 1000)
+            }
+          }
+        },
+          fail: function (res) {
+            console.log(res)   
+          }
+      })
+    }else{
+      callback(res);
+    }
+   },
+   fail: function (err) {
+    console.log(err)
+    errFun(err);
+   },
+   complete:(res) => {
+    wx.hideLoading()
+  }
+  })
+ },
     globalData: {
       ColorList: [{
         title: '嫣红',
@@ -100,9 +183,9 @@ App({
     ],
     userInfo: null,
     requestUrl: 'https://wxp.diaochaonline.com'//35
-   // requestUrl: 'https://wmccpr.diaochaonline.com'//线上
-   //requestUrl:'https://cmmrpr.diaochaonline.com'//13o
-      //requestUrl:'http://221.216.95.200:8286'//35
-    //requestUrl:'http://192.168.20.78:8188'//本地
+    //requestUrl: 'https://wmccpr.diaochaonline.com'//线上
+    //requestUrl:'https://cmmrpr.diaochaonline.com'//13o
+    //requestUrl:'http://221.216.95.200:8286'//35
+    //requestUrl:'http://192.168.20.78:8190'//本地
   }
 })
